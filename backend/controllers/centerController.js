090cb759f2678620c069e88986b2038014915d91
Najat zaidane <najatzaidane2002@outlook.com>
2024-04-11 19:20:35 +0100

    1

--- backend/controllers/centerController.js
+++ backend/controllers/centerController.js
@@ -1,37 +1,39 @@
 //this file contains the functions to manage the centers routes 
 
+const asyncHandler = require('express-async-handler');
+
 // @desc Get center
 // @route  GET /api/centers
 // @access Private
-const getCenters = (req, res) => {
+const getCenters = asyncHandler (async (req, res) => {
     res.status(200).json({message : 'Get centers' })
-}
+})
 
 // @desc Set center
 // @route  POST /api/centers
 // @access Private
-const setCenter = (req, res) => {
+const setCenter = asyncHandler (async (req, res) => {
     if(!req.body.text) {
         res.status(400)
         throw new Error('Please add a text field')
     }
 
     res.status(200).json({message : 'Set center' })
-}
+} ) 
 
 // @desc Update center
 // @route  Put /api/centers/:id
 // @access Private
-const updateCenter = (req, res) => {
+const updateCenter = asyncHandler (async (req, res) => {
     res.status(200).json({message : `Update center ${req.params.id}` })
-}
+} ) 
 
 // @desc Delete center
 // @route  DELETE /api/centers/:id
 // @access Private
-const deleteCenter = (req, res) => {
+const deleteCenter = asyncHandler ( async (req, res) => {
     res.status(200).json({message : `Delete center ${req.params.id}`  })
-}
+} ) 
 
 module.exports = {
     getCenters,
