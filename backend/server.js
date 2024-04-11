8e47c89155d602451e7488616dea41636cd51dff
Najat zaidane <najatzaidane2002@outlook.com>
2024-04-11 22:24:15 +0100

    1

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,10 @@ const express =  require('express')
 const colors =  require ( 'colors')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const port = process.env.port  || 5000
+
+connectDB()
 
 const app = express()
 
