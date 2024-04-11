1afda8d8c339162d5dc76f5bdf7c4cd03f60530f
Najat zaidane <najatzaidane2002@outlook.com>
2024-04-11 18:04:33 +0100

    1

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -6,6 +6,10 @@ const errorHandler = (err, req, res, next) => {
 
     res.json({
         message: err.message,
-        stack: process.env.NODE_ENV === 'production' ? nul
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
     })
+}
+
+module.exports = {
+    errorHandler,
 }
