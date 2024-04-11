42e4ec076156c4442872285c12dff72e5715abed
Najat zaidane <najatzaidane2002@outlook.com>
2024-04-11 22:52:26 +0100

    1

---
+++ backend/model/centerModel.js
@@ -0,0 +1,8 @@
+//in this folder model, we're gonna define any ressource that we have
+//in this file we're gonna define the schema wich gonna be the fields for this particular ressource ( goal) 
+
+const  mongoose = require('mongoose')
+
+const centerSchema = mongoose.Schema({
+    
+})
