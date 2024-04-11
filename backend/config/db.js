375ae50df245046334c3cdb8d79c286e2752f0c6
Najat zaidane <najatzaidane2002@outlook.com>
2024-04-11 22:11:30 +0100

    1

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,5 +2,16 @@ const mongoose = require ('mongoose')
 
 //all the mongoose methods are asynchronised : they return a promise
 const connectDB = async () => {
+    try {
+        //connect 
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
 
 }
+
+module.exports = connectDB
