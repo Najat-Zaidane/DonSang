a59aeba29582466b61d38d54830a40e211b9ab56
Najat zaidane <najatzaidane2002@outlook.com>
2024-04-12 21:18:02 +0100

    1

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,8 @@
-const mongoose = require ('mongoose')
+//tp connect to the database 
 
 //all the mongoose methods are asynchronised : they return a promise
 const connectDB = async () => {
-    try {
-        //connect 
-        const conn = await mongoose.connect(process.env.MONGO_URI)
-
-        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
-    } catch (error) {
-        console.log(error)
-        process.exit(1)
-    }
-
+   
 }
 
 module.exports = connectDB
--- backend/model/centerModel.js
+++ backend/model/centerModel.js
@@ -1,23 +1,3 @@
 //in this folder model, we're gonna define any ressource that we have
 //in this file we're gonna define the schema wich gonna be the fields for this particular ressource ( goal) 
 
-const  mongoose = require('mongoose')
-
-
-const centerSchema = mongoose.Schema({
-    nom: {
-        type : String,
-        required : [true , 'le nom est obligatoire']
-    },
-    adresse : {
-        type : String,
-        required : [true,'l\'adresse est obligatoire']
-    },
-    heuresOuvertures : {
-        type : String,
-        default : "09h-12h à 14h-17h",
-        required : [true,'les horaires sont obligatoires']
-    },
-})
-
-module.exports = mongoose.model('center', centerSchema)
