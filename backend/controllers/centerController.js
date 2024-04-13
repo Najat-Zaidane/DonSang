dfa6e80633e150313d5e6b075b4a6e435d022fa7
Najat zaidane <najatzaidane2002@outlook.com>
2024-04-13 23:57:04 +0100

    1

--- backend/controllers/centerController.js
+++ backend/controllers/centerController.js
@@ -3,24 +3,26 @@
 const asyncHandler = require('express-async-handler') // this function is used to simplify errors handling for asynchronous  functions that returns promises
 const Center = require('../model/centerModel');
 
+// @desc Set center
+// @route  POST /api/centers
+// @access Private
+const setCenter = asyncHandler (async (req, res) => {
+    try {
+        const { nom, adresse, email } = req.body;
+        const center = await Center.create({ nom, adresse, email });
+        res.status(200).json(center)
+    } catch (error) {
+        res.status(401)
+    }
+   
+} ) 
+
 // @desc Get center
 // @route  GET /api/centers
 // @access Private
 const getCenters = asyncHandler (async (req, res) => {
     res.status(200).json({message : 'Get centers' })
 })
-
-// @desc Set center
-// @route  POST /api/centers
-// @access Private
-const setCenter = asyncHandler (async (req, res) => {
-
-    const { nom, adresse, email } = req.body;
-
-    const center = await Center.create({ nom, adresse, email });
-    
-    res.status(200).json(center)
-} ) 
 
 // @desc Update center
 // @route  Put /api/centers/:id
