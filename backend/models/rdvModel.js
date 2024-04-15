e498ff78b53743bd8f4154205645dba8bf78144f
Najat zaidane <najatzaidane2002@outlook.com>
2024-04-15 23:56:02 +0100

    1

--- backend/models/rdvModel.js
+++ backend/models/rdvModel.js
@@ -1,55 +1,47 @@
-const Sequelize = require('sequelize')
-const sequelize = require('../config/dbConfig')
+const Sequelize = require('sequelize');
+const sequelize = require('../config/dbConfig'); // Assuming your config file path
 const User = require('./userModel')
 const Center = require('./centerModel')
 const Creneau = require('./creneauModel')
 
 const Rdv = sequelize.define("rdv", {
-        userId : {
-            type : Sequelize.INTEGER,
-            allowNull : false,
-            references : {
-                model : User,
-                key : 'id',
-            }
-        },
-        centerId : {
-            type : Sequelize.INTEGER,
-            allowNull: false ,
-            references : {
-                model : Center,
-                key : 'id'
-            }
-        },
-        creneauId : {
-            type : Sequelize.INTEGER,
-            allowNull: false ,
-            references : {
-                model : Creneau,
-                key : 'id'
-            }
-        },
-        dateTime : {
-            type: Sequelize.DATE, //DATE is a sequelize type that represents both date and time
-            allowNull: false ,
-        }, 
-        status : { // appointement status is : validé , non validé , annulé 
-            type : Sequelize.STRING,
-            allowNull: false ,
+  userId: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: {
+      model: User,
+      key: 'id',
+    },
+  },
+  centerId: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: {
+      model: Center,
+      key: 'id',
+    },
+  },
+  creneauId: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: {
+      model: Creneau,
+      key: 'id',
+    },
+ },
+  dateTime: {
+    type: Sequelize.DATE, // Use DATE for both date and time
+    allowNull: false,
+  },
+  status: { // Appointment status
+    type: Sequelize.STRING,
+    allowNull: false,
+    values: ['validé', 'non validé', 'annulé'], // Restrict status values
+  },
+});
 
-        },
-})
-
-//define the relations 
-
-//rdv and user : one-to-one relation
-//each rdv is associated with one user
-//Rdv.belongsTo(User, { foreignKey : 'userId'}) 
-
-//rdv and creneau : one-to-one relation
-//each rdv is associated with one creaneau
-Rdv.belongsTo(Creneau, {foreignKey : 'creneauId'})
+// Associations
+Rdv.belongsTo(Creneau, { foreignKey: 'creneauId' });
 
 
-
-module.exports = Rdv ;
+module.exports = Rdv;
