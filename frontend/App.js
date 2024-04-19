ed0246a2db3476df4598fca2e14780a3dc4a296e
Najat zaidane <najatzaidane2002@outlook.com>
2024-04-19 20:16:07 +0100

    1

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native';
 
+
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello i'm Najwa</Text>
-   
     </View>
   );
 }
