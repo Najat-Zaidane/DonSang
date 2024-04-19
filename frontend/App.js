3ffef89517cce1512b32463b43d70b4f36247a4b
Najat zaidane <najatzaidane2002@outlook.com>
2024-04-19 20:25:36 +0100

    1

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
+//The entry point to the app 
+import Register from "./screens/Register";
 
 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello i'm Najwa</Text>
-    </View>
-  );
+   <Register/>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- frontend/screens/Register.js
+++ frontend/screens/Register.js
@@ -1,13 +1,24 @@
 import React  from "react";
-import {Text, View} from 'react-native'
+import {Text, View, StyleSheet,TextInput} from 'react-native'
 
 const Register = () => {
     return(
-        <View>
-            <Text>This is the Register Page.</Text>
+        <View style={styles.container}>
+            <Text style= {styles.text}>This is the Register Page.</Text>
         </View>
     )
 
 }
 
 export default Register;
+
+const styles = StyleSheet.create({
+    container : {
+        flex : 1,
+        justifyContent : 'center',
+    },
+    text : {
+        fontSize : 24,
+        color: '#333',
+    }
+})
