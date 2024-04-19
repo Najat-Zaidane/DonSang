4a3b7e60d6e9f3df51218550477b24ea1a950bf5
Najat zaidane <najatzaidane2002@outlook.com>
2024-04-20 00:48:50 +0100

    1

--- frontend/App.js
+++ frontend/App.js
@@ -1,13 +1,14 @@
 //The entry point to the app 
 import Register from "./screens/Register";
-import Login  from  "./screens/Login" ;
+import Login from "./screens/Login"
 
 
 
 export default function App() {
   return (
-   //<Register/>
+  //  <Register/>
   <Login/>
+  
   )
 }
 
