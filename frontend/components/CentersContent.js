1c76b661bb21f4b7e6a0b603f7c59d53dc3506ec
Najat zaidane <najatzaidane2002@outlook.com>
2024-04-25 00:59:45 +0100

    1

--- frontend/components/CentersContent.js
+++ frontend/components/CentersContent.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { View,ScrollView, Text, StyleSheet, Image } from "react-native";
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import axios from 'axios';
+import { SearchBar } from "react-native-screens";
 
 const API_URL_centers = 'http://192.168.1.101:5000/api/centers/'
 
 const  CentersContent = () => {
+
+    const [centers, setCenters] = useState([]);
+
+    const [keyword,setKeyword] = useState('');
+
+    // Fonction pour mettre à jour le texte de recherche
+    const handleSearch = (text) => {
+        setKeyword(text);
+    };
+
+    // Filtrer les centres en fonction du texte de recherche
+    const filteredCenters = centers.filter(center =>
+        center.nom.toLowerCase().includes(keyword.toLowerCase())
+    );
 
     //récupération des centres de santé 
 
@@ -12,7 +28,7 @@ const  CentersContent = () => {
         try {
             const response = await axios.get(API_URL_centers)
             console.log('centersinCENTERSCREEN', response.data)
-           // setCenters(response.data);  // mise à jour du state centers avec les données de l'API
+           setCenters(response.data);  // mise à jour du state centers avec les données de l'API
         } catch (error) {
             console.error('Failed to fetch centers:', error)
         }
@@ -27,16 +43,40 @@ const  CentersContent = () => {
     return (
         <ScrollView style={styles.container} >
 
-        <View style={styles.logoContainer}>
+        {/* <View style={styles.logoContainer}>
           <Image
            source={require('../assets/logo2.png')} 
            style={styles.logo}          
           />
-        </View>
+        </View> */}
 
+       
+            
             <Text style={styles.title}>Découvrez les centres de santé disponibles</Text>
 
             
+{/* Barre de recherche */}
+                <SearchBar
+                placeholder="Rechercher un centre"
+                onChangeText={handleSearch}
+                value={keyword}
+                style={styles.searchbar}
+            />
+         
+
+           {/* Liste des centres filtrés */}
+           {filteredCenters.map(center => (
+                <View key={center.id} style={styles.section}>
+                    <Text style={styles.sectionTitle}>
+                        <FontAwesome5 name="hospital" size={15} style={styles.icon} />
+                        {center.nom}
+                    </Text>
+                    <Text style={styles.sectionText}>Adresse : {center.adresse}</Text>
+                    <Text style={styles.sectionText}>Email : {center.email}</Text>
+                </View>
+            ))}
+            
+
 
         </ScrollView>
     );
@@ -87,6 +127,10 @@ const styles = StyleSheet.create({
      width : 160,
      height : 80,
      resizeMode:"contain",
+    },
+    searchbar: {
+        marginTop: 10,
+        marginBottom: 10,
     },
 });
 
